a33ff359031c20d4677b4c249e85da9d7f7a6fd3
ahmed.kamar <ahmed.shaban.kamar@gmail.com>
2024-12-21 12:30:23 +0200

    feat(project): add subscribe

---
+++ src/app/subscribe/subscribe.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subscribe',
+  templateUrl: 'subscribe.page.html',
+  styleUrls: ['subscribe.page.scss'],
+  imports: [],
+})
+export class SubscribePage {
+  constructor() {}
+}
